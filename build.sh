d394ae7287318ce9345bf17bd1bfbca3008e9d17
Cristian Romanescu <cristian@romanescu.ro>
2018-06-22 12:27:00 +0300

    Add script to build images

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$1" == "" ]; then
+	echo "Example usage: ./test-build.sh 1.0.3"
+	exit 1
+fi
+
+
+echo "────────────────────────────────────────────────────"
+echo "   Building image for eaudeweb/php-fpm:5.6-$1"
+echo "────────────────────────────────────────────────────"
+docker build --rm -t eaudeweb/php-fpm:5.6-$1 ./5.6/
+
+echo "────────────────────────────────────────────────────"
+echo "   Building image for eaudeweb/php-fpm:7.1-$1"
+echo "────────────────────────────────────────────────────"
+docker build --rm -t eaudeweb/php-fpm:7.1-$1 ./7.1/
+
+echo "────────────────────────────────────────────────────"
+echo "   Building image for eaudeweb/php-fpm:7.2-$1"
+echo "────────────────────────────────────────────────────"
+docker build --rm -t eaudeweb/php-fpm:7.2-$1 ./7.2/
